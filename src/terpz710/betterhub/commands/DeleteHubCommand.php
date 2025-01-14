75fdaaef979d3ce58f9093e4c2fc33d9d512246c
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-01-13 22:39:04 -0800

    Update DeleteHubCommand.php

--- src/terpz710/betterhub/commands/DeleteHubCommand.php
+++ src/terpz710/betterhub/commands/DeleteHubCommand.php
@@ -21,8 +21,9 @@ class DeleteHubCommand extends Command implements PluginOwned {
     private $plugin;
 
     public function __construct() {
-        parent::__construct("deletehub");
-        $this->setDescription("Delete the hub location");
+        parent::__construct(Hub::getInstance()->getConfig()->get("deletehub-command-label"));
+        $this->setDescription(Hub::getInstance()->getConfig()->get("deletehub-command-description"));
+        $this->setAliases(Hub::getInstance()->getConfig()->get("deletehub-command-aliases"));
         $this->setPermission(Permission::PERM_DELHUB);
 
         $this->plugin = Hub::getInstance();
