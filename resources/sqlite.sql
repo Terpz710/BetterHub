e5bc5a253774b5d78d3d8037e1e9280dfae6a93a
WavyCraftNetwork <142659461+Terpz710@users.noreply.github.com>
2025-02-09 00:30:23 -0800

    Update sqlite.sql

--- resources/sqlite.sql
+++ resources/sqlite.sql
@@ -7,7 +7,7 @@
             x REAL NOT NULL,
             y REAL NOT NULL,
             z REAL NOT NULL,
-            world TEXT NOT NULL
+            world TEXT NOT NULL UNIQUE
         );
     -- #}
 -- #}
@@ -19,12 +19,16 @@
             x REAL NOT NULL,
             y REAL NOT NULL,
             z REAL NOT NULL,
-            world TEXT NOT NULL
+            world TEXT NOT NULL UNIQUE
         );
     -- #}
 
-    -- #{ delete
-        DELETE FROM hub;
+    -- #{ update
+        -- # :x float
+        -- # :y float
+        -- # :z float
+        -- # :world string
+        UPDATE hub SET x = :x, y = :y, z = :z WHERE world = :world;
     -- #}
 
     -- #{ insert
@@ -33,7 +37,8 @@
         -- # :z float
         -- # :world string
         INSERT INTO hub (x, y, z, world)
-        VALUES (:x, :y, :z, :world);
+        VALUES (:x, :y, :z, :world)
+        ON CONFLICT(world) DO UPDATE SET x = excluded.x, y = excluded.y, z = excluded.z;
     -- #}
 
     -- #{ select
